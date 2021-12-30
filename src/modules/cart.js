3bc0a359689105a05b3812d32a850c9c6c87357d
In5lux <nikos_almeryda@mail.ru>
2021-12-31 00:51:43 +0300

    Render cart

--- src/modules/cart.js
+++ src/modules/cart.js
@@ -1,15 +1,65 @@
+import renderCart from './renderCart'
+import { price } from './price';
+import postData from './postData';
+
 const cart = () => {
-  const cartBtn = document.querySelector("#cart");
-  const cart = document.querySelector(".cart");
-  const cartCloseBtn = document.querySelector(".cart-close");
+	const cartBtn = document.querySelector('#cart');
+	const cartModal = document.querySelector('.cart');
+	const cartCloseBtn = cartModal.querySelector('.cart-close');
+	const cartTotal = cartModal.querySelector('.cart-total > span');
+	const cartSendBtn = cartModal.querySelector('.cart-confirm');
+	const goodsWrapper = document.querySelector('.goods');
+	const cartWrapper = document.querySelector('.cart-wrapper');		
 
-  cartBtn.addEventListener("click", () => {
-    cart.style.display = "flex";
-  });
+	cartBtn.addEventListener("click", () => {
+		cartModal.style.display = "flex";
+		const cartItems = localStorage.getItem('cart') && !undefined ? JSON.parse(localStorage.getItem('cart')) : [];
+		renderCart(cartItems);
+		cartTotal.textContent = price(cartItems.reduce((sum,goodItem)=>sum+goodItem.price,0));
+		
+		cartCloseBtn.addEventListener("click", () => {
+			cartModal.style.display = "";
+	});
+	})
 
-  cartCloseBtn.addEventListener("click", () => {
-    cart.style.display = "";
-  });
-};
+	goodsWrapper.addEventListener('click',(e)=>{			
+    if (e.target.classList.contains('btn-primary')) {		
+		const card = e.target.closest('.card');
+		const key = card.dataset.key;
+		const goods = JSON.parse(localStorage.getItem('goods'));
+		const cartItems = localStorage.getItem('cart') ? JSON.parse(localStorage.getItem('cart')) : [];
+		const goodItem = goods.find(item=>item.id===+key);
+		cartItems.push(goodItem);			
+		localStorage.setItem('cart', JSON.stringify(cartItems));
+    	};
+	});
+
+	cartWrapper.addEventListener('click',(e)=>{
+	if (e.target.classList.contains('btn-primary')) {		
+		const cartItems = localStorage.getItem('cart') ? JSON.parse(localStorage.getItem('cart')) : [];
+		const card = e.target.closest('.card');
+      	const key = card.dataset.key;
+		const index = cartItems.findIndex(item=>item.id ===+key);
+
+		cartItems.splice(index,1);
+
+		localStorage.setItem('cart', JSON.stringify(cartItems));  
+		console.log(cartItems);
+		renderCart(cartItems);
+		cartTotal.textContent = price(cartItems.reduce((sum,goodItem)=>sum+goodItem.price,0));
+		}		
+  	});
+
+	cartSendBtn.addEventListener('click',()=>{
+		const cartItems = localStorage.getItem('cart') ? JSON.parse(localStorage.getItem('cart')) : [];
+
+		postData(cartItems).then(()=>{
+			localStorage.removeItem('cart');
+			renderCart([]);
+			cartTotal.textContent = 0;
+		});
+	});
+
+}
 
 export default cart
--- src/modules/postData.js
+++ src/modules/postData.js
@@ -1,14 +1,7 @@
-const postData = () => {
-	return fetch('http://localhost:3000/goods', {
+const postData = (cart) => {
+	return fetch('https://jsonplaceholder.typicode.com/posts', {
 		method: 'POST',
-		body: JSON.stringify({
-			id: "26",
-			title: "TEST",
-			price: 1794,
-			sale: true,
-			img: "https://cdn1.ozone.ru/multimedia/c400/1023547851.jpg",
-			category: "И000000",
-	}),
+		body: JSON.stringify(cart),
   		headers: {
 		'Content-type': 'application/json; charset=UTF-8',
 		},
